438e868e4479964dec88a8a7b93f266aa064b522
Ali Safari <Asafarim@gmail.com>
2025-07-10 14:21:18 +0200

    chore: add Vite config for sidebar demo with custom base path and build settings

--- packages/sidebar/demo/vite.config.ts
+++ packages/sidebar/demo/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-packages/asafarim/sidebar/',
+  base: '/asafarim-webapp/my-packages/asafarim/sidebar/demo/',
   server: {
     port: 3030,
     open: true
