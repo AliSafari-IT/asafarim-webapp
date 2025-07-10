b64708c4f01cc1c8ee645868d3ebcdf1da50521f
Ali Safari <Asafarim@gmail.com>
2025-07-10 14:33:39 +0200

    Fix base path in Vite config for GitHub Pages deployment

--- packages/sidebar/demo/vite.config.ts
+++ packages/sidebar/demo/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/asafarim-webapp/my-packages/asafarim/sidebar/demo/',
+  base: '/my-packages/asafarim/sidebar/',
   server: {
     port: 3030,
     open: true
